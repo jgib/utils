351511832de7113660b333d2856946ca68d16e58
gibersonja <gibersonja@gmail.com>
2025-06-05 12:23:37 -0500

    testing

--- util.go
+++ util.go
@@ -90,9 +90,9 @@ func PP[T any](input *T) string {
 		fmt.Println("SLICE")
 	} else if dataType[:3] == "map" {
 		fmt.Println("MAP")
-    } else if dataType[:3] == "int" || dataType[1:4] == "uint" || dataTyep[:5] == "float" || dataType[:7] == "complex" {
-        output = fmt.Sprintf("(%T) %d, *input)
-    } else {
+	} else if dataType[:3] == "int" || dataType[1:4] == "uint" || dataTyep[:5] == "float" || dataType[:7] == "complex" {
+		output = fmt.Sprintf("(%T) %d", *input)
+	} else {
 
 		switch fmt.Sprintf("%T", *input) {
 		case "int8":
